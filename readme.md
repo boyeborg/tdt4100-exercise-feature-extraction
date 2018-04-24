f639be7b16341958fb57f8c8c7d3868e9f2f4c67
Boye Borg <boye.borg@icloud.com>
2018-04-24 15:29:10 +0200

    initial commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+# TDT4100 exercise feature extraction
+
+Feature extraction from the programming process of students solving exercises in the course TDT4100
+at NTNU.
---
+++ src/main/java/com/github/boyeborg/App.java
@@ -0,0 +1,16 @@
+package com.github.boyeborg;
+
+/**
+ * Hello world class.
+ */
+public class App {
+
+	/**
+	 * Hello world method.
+	 * 
+	 * @param args Arguments passed from the command line
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
